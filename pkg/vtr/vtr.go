7afd3d759c72e1328b9c818f12157118b3b7d3cb
Thai Pangsakulyanont @ Taskworld <thai.p@taskworld.com>
2018-07-06 16:45:14 +0700

    :bug: Fix VTR, it is now working

--- pkg/vtr/vtr.go
+++ pkg/vtr/vtr.go
@@ -2,6 +2,7 @@ package vtr
 
 import (
 	"os/exec"
+	"sync"
 	"syscall"
 
 	log "github.com/sirupsen/logrus"
@@ -10,9 +11,10 @@ import (
 
 // VTR represents a running instance of a video recorder process.
 type VTR struct {
-	cmd      *exec.Cmd
-	status   string
-	listener ProgressListener
+	finished chan bool
+	wg       sync.WaitGroup
+	// cmd      *exec.Cmd
+	// listener ProgressListener
 }
 
 type ProgressListener interface {
@@ -29,71 +31,80 @@ func StartRecordingVideo(listener ProgressListener) *VTR {
 	if listener != nil {
 		listener.SetStatus("launching")
 	}
-	cmd := exec.Command(
-		"ffmpeg",
-		"-video_size", "1280x1024",
-		"-framerate", "15",
-		"-f", "x11grab",
-		"-i", ":99.0",
-		"/videos/video.mp4",
-	)
-	cmdlogger.LogCommandOutput(myLog.WithField("cmd", "ffmpeg"), cmd)
 
-	err := cmd.Start()
-	if err != nil {
-		myLog.Fatal("Cannot start video recording process:", err)
+	vtr := &VTR{
+		finished: make(chan bool, 1),
+		wg:       sync.WaitGroup{},
 	}
+	vtr.wg.Add(1)
 
-	myLog.Info("Video recording started...")
-	if listener != nil {
-		listener.SetStatus("recording")
-	}
-	return &VTR{
-		cmd:      cmd,
-		listener: listener,
-	}
+	go func() {
+		vtr.wg.Done()
+		cmd := exec.Command(
+			"ffmpeg",
+			"-video_size", "1280x1024",
+			"-framerate", "15",
+			"-f", "x11grab",
+			"-i", ":99.0",
+			"/videos/video.mp4",
+		)
+		cmdlogger.LogCommandOutput(myLog.WithField("cmd", "ffmpeg"), cmd)
+
+		err := cmd.Start()
+		if err != nil {
+			myLog.Fatal("Cannot start video recording process:", err)
+		}
+
+		myLog.Info("Video recording started...")
+		if listener != nil {
+			listener.SetStatus("recording")
+		}
+
+		<-vtr.finished
+		myLog.Info("Stopping video recording.")
+		if listener != nil {
+			listener.SetStatus("stopping")
+		}
+		if err := cmd.Process.Signal(syscall.SIGINT); err != nil {
+			if listener != nil {
+				listener.SetStatus("error")
+			}
+			myLog.Error("Cannot send SIGINT to ffmpeg process: ", err)
+		}
+		if err := cmd.Wait(); err != nil {
+			myLog.Info("ffmpeg exited: ", err)
+		}
+		myLog.Info("Finalizing the video.")
+		if listener != nil {
+			listener.SetStatus("finalizing")
+		}
+		cmd = exec.Command(
+			"MP4Box",
+			"-isma",
+			"-inter", "500",
+			"video.mp4",
+		)
+		cmd.Dir = "/videos"
+		cmdlogger.LogCommandOutput(myLog.WithField("cmd", "MP4Box"), cmd)
+		if err := cmd.Run(); err != nil {
+			myLog.Error("Finalization error: ", err)
+			if listener != nil {
+				listener.SetStatus("error")
+			}
+			return
+		}
+		myLog.Info("Video finalization finished.")
+		if listener != nil {
+			listener.SetStatus("finished")
+		}
+	}()
+
+	return vtr
 }
 
 // StopRecordingVideo tells the video recording process to stop recording
 // video and blocks until video is converted successfully.
-func (v *VTR) StopRecordingVideo() error {
-	myLog.Info("Stopping video recording.")
-	listener := v.listener
-	if listener != nil {
-		listener.SetStatus("stopping")
-	}
-	if err := v.cmd.Process.Signal(syscall.SIGINT); err != nil {
-		if listener != nil {
-			listener.SetStatus("error")
-		}
-		myLog.Error("Cannot send SIGINT to ffmpeg process: ", err)
-		return err
-	}
-	if err := v.cmd.Wait(); err != nil {
-		myLog.Info("ffmpeg exited: ", err)
-	}
-	myLog.Info("Finalizing the video.")
-	if listener != nil {
-		listener.SetStatus("finalizing")
-	}
-	cmd := exec.Command(
-		"MP4Box",
-		"-isma",
-		"-inter", "500",
-		"/videos/video.mp4",
-	)
-	cmdlogger.LogCommandOutput(myLog.WithField("cmd", "MP4Box"), cmd)
-	if err := cmd.Run(); err != nil {
-		myLog.Error("Finalization error: ", err)
-		listener.SetStatus("error")
-		return err
-	}
-	if listener != nil {
-		listener.SetStatus("finished")
-	}
-	return nil
-}
-
-func (v *VTR) setStatus(text string) {
-	v.status = text
+func (v *VTR) StopRecordingVideo() {
+	v.finished <- true
+	v.wg.Wait()
 }
